9171a1accc9305981a652c038b7dce86c7d0d08d
lucasfrosty <lucas_frosty@hotmail.com>
2018-04-15 18:31:21 -0300

    v1.0.5

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 module.exports = {
   "extends": [
-    "airbnb", "prettier", "prettier/react"
+    "airbnb"
   ],
-  "plugins": [ "prettier" ],
   "parser": "babel-eslint",
   "env": {
     "browser": true,
