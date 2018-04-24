f8fd5ce992484b9cdb5b4b2df01d2a0af7c6f301
lucasfrosty <lucas_frosty@hotmail.com>
2018-04-24 15:45:19 -0300

    using shopify's styleguide now

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 module.exports = {
   "extends": [
-    "airbnb"
+    "plugin:shopify/esnext",
+    "plugin:shopify/react",
   ],
   "parser": "babel-eslint",
   "env": {
     "browser": true,
     "node": true
-  },
-  "rules": {
-    "react/jsx-filename-extension": 0,
-    "camelcase": "off",
-    "jsx-a11y/anchor-is-valid": [
-      "error",
-      {
-        "components": [
-          "Link"
-        ],
-        "specialLink": [
-          "to",
-          "hrefLeft",
-          "hrefRight"
-        ],
-        "aspects": [
-          "noHref",
-          "invalidHref",
-          "preferButton"
-        ]
-      }
-    ]
   }
 }
